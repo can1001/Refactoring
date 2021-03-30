282c7222e5876567efb3c58f1804236ccfd6de21
can1001 <hyejong.jeong@gmail.com>
2021-03-30 21:25:06 +0900

    메서드이동(Move Method)

--- Customer.java
+++ Customer.java
@@ -28,7 +28,7 @@ public class Customer {
             Rental each = (Rental) rentals.nextElement();
 
             // 비디오 종류별 대여로 계산
-            thisAmount = amountFor(each);
+            thisAmount = each.getCharge();
 
             // 적립 포인트를 1 포인트 증가
             frequentRenterPoints ++;
@@ -50,27 +50,5 @@ public class Customer {
         result += "적립 포인트: " + String.valueOf(frequentRenterPoints);
         return result;
     }
-
-    // 비디오 종류별 대여로 계산 기능을 빼내어 별도의 함수로 작성
-    private double amountFor(Rental aRental) {
-        double result = 0;
-        switch (aRental.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                result +=2;
-                if (aRental.getDaysRented() > 2) {
-                    result += (aRental.getDaysRented() - 2) * 1.5;
-                }
-                break;
-            case Movie.NEW_RELEASE:
-                result += aRental.getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (aRental.getDaysRented() > 3) {
-                    result += (aRental.getDaysRented() -3) * 1.5;
-                }
-                break;
-        }
-        return result;
-    }
+        
 }
